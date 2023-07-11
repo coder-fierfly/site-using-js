21d00037e5489b18c05efc41211b5a6d0b7e020b
lena <andrianovalena1@gmail.com>
2023-07-11 18:34:39 +0300

    Сделала поиск по тексту более точным

--- src/components/TextAreaFilter.jsx
+++ src/components/TextAreaFilter.jsx
@@ -1,17 +1,18 @@
-import Fuse from 'fuse.js';
+import MatchMapping from "../MatshMapping";
 
 function TextAreaFilter(text, array) {
     if (text === '') {
         return array;
     }
-    const fuse = new Fuse(array, {
-        keys: ['target_name', 'name'], // Specify the fields in your data that should be searchable
-        includeScore: true, // Enables scoring for search results
-        threshold: 0.3, // Adjust the threshold to control the accuracy of the search
-    });
     var results = []
-    fuse.search(text).forEach(el =>
-        results.push(el.item))
+
+    const searchRegex = new RegExp(text, 'i');
+
+    array.map(element => {
+        if (searchRegex.test(element.target_name) || searchRegex.test(element.name) || searchRegex.test(MatchMapping[element.state].color)) {
+            results.push(element);
+        }
+    });
 
     return results;
 }
