53d5ee03b03dece2e9c1af0de24672ad208a334a
lena <andrianovalena1@gmail.com>
2023-07-14 14:18:11 +0300

    Сделала открытие карточки и переписала скроллбар

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import json from "./jsonConsole.json";
 import MatchMapping from "../MatshMapping";
 
@@ -7,64 +7,177 @@ var quotList = json.data.alerts;
 
 var cardList = json.data.alerts;
 const matrixArray = [];
-// const TimeFilter = require('./TimeFilter.jsx');
+
+
+// чтобы страница не уезжала
+//сделать основное меню расширяемым
+// неделя
+// месяц год    
 
 
 
 const Cards = (props) => {
-    const currentTime = Date.now();
+
+    const [selectedItem, setSelectedItem] = useState(null);
+    const [showPopupLink, setShowPopup] = useState(false);
+
+
+
+    const [expandedElements, setExpandedElements] = useState([]);
+
+    const toggleElement = (id) => {
+        if (expandedElements.includes(id)) {
+            setExpandedElements(expandedElements.filter((elementId) => elementId !== id));
+        } else {
+            setExpandedElements([...expandedElements, id]);
+        }
+    };
+
+
+
+
+
+    const handleDivClick = (item) => {
+        setSelectedItem(item);
+    };
+
+    const handleCloseClick = () => {
+        setSelectedItem(null);
+    };
+
+    const handleLinkClick = () => {
+        console.log('работаю')
+        setShowPopup(true);
+    };
+
+    const handleCloseLinkClick = () => {
+        setShowPopup(false);
+    };
+
+
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    // const handleClick = () => {
+    //     setIsExpanded(!isExpanded);
+    // };
+    const [selectedIndex, setSelectedIndex] = useState(null);
+    const handleClick = (index) => {
+        // Если элемент уже выбран, сбрасываем индекс в null
+        if (selectedIndex === index) {
+            setSelectedIndex(null);
+        } else {
+            // Иначе устанавливаем индекс в значение параметра
+            setSelectedIndex(index);
+        }
+    };
+
+    function timeConverter(time, interval) {
+        const convertDate = new Date(time * 1000);
+        if (interval === 'withinHour') {
+            const currentTime = Date.now();
+            var min = Math.floor((currentTime - time * 1000) / 60000);
+            return min.toString() + ' мин назад';
+        }
+
+        if (interval === 'yesterday') {
+            const formattedDate = convertDate.toLocaleString('en-GB', {
+                day: '2-digit',
+                month: '2-digit',
+                year: '2-digit',
+                hour: 'numeric',
+                minute: 'numeric',
+                second: 'numeric',
+                hour12: false
+            }).replace(/\//g, '.').replace(/,/g, '');
+            return formattedDate.toString();
+        }
+        if (interval === 'today') {
+            const formattedTime = convertDate.toLocaleString('en-US', {
+                hour: 'numeric',
+                minute: 'numeric',
+                second: 'numeric',
+                hour12: false
+            }).replace(/\//g, '.').replace(/,/g, '');
+            return formattedTime.toString();
+        }
+    }
+
+
     return (
         <div>
             {props.itemValue.length === 0 ? (
                 <div className="withoutColor">Нет подходящих элементов</div>
             ) : (
                 props.itemValue.map((item) => {
+
                     var col = MatchMapping[item.state].color;
-                    var minutesAgo = Math.floor((currentTime - item.time_value * 1000) / 60000);
-                    const cd = new Date(item.time_value * 1000);
-                    const formattedDate = cd.toLocaleString('en-US', {
-                        day: '2-digit',
-                        month: '2-digit',
-                        year: 'numeric',
-                        hour: 'numeric',
-                        minute: 'numeric',
-                        second: 'numeric',
-                        hour12: false
-                    }).replace(/\//g, '.').replace(/,/g, '');
-
-                    const formattedTime = cd.toLocaleString('en-US', {
-                        hour: 'numeric',
-                        minute: 'numeric',
-                        second: 'numeric',
-                        hour12: false
-                    }).replace(/\//g, '.').replace(/,/g, '');
-
                     return (
-                        <div className="card" key={item.event_id}   >
-                            {/* <div className="color"> */}
-                            <div className="board_line" style={{ background: col }}></div>
-                            <div className="signal_color"  >
-                                <div className="circle" style={{ background: col }}></div>
-                                <div className="line" style={{ borderLeft: "1.5px dashed" + col }}></div>
+                        <>
+                            {/* onClick={() => handleDivClick(item)}     */}
+                            <div className="card" key={item.event_id} >
+                                <>
+                                    { }
+                                    <button className="close-button" onClick={() => handleClick(item.event_id)}>
+                                        {selectedIndex === null ? (<div className="close_popup_btn">
+                                            <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16"><path d="M480-345 240-585l43-43 197 198 197-197 43 43-240 239Z" /></svg>
+                                        </div>) : (<div className="close_popup_btn">
+                                            <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16"><path d="m283-345-43-43 240-240 240 239-43 43-197-197-197 198Z" /></svg>
+                                        </div>)}
+
+                                    </button>
+                                    <div className="board_line" style={{ background: col }}></div>
+                                    <div className="signal_color"  >
+                                        <div className="circle" style={{ background: col }}></div>
+                                        <div className="line" style={{ borderLeft: "1.5px dashed" + col }}></div>
+                                    </div>
+
+                                    <div className="text_group">
+
+                                        <p className="item_1">{MatchMapping[item.state].description}</p>
+                                        {/* {(selectedIndex === item.event_id) ? (<div>he</div>) : (<div>asdasd</div>)} */}
+
+                                        {(selectedIndex === item.event_id && item.name !== '') ?
+                                            (<div className="container"><p><span className="title_element">Имя устройства:&nbsp;</span>
+                                                <span className="item_element">{item.name}</span></p></div>) : (item.name === '' ? (null) : (<p className="item_2">{item.name}</p>))}
+
+                                        {(selectedIndex === item.event_id && item.target_name !== '') ?
+                                            (<div className="container"><p><span className="title_element">Имя целевого объекта:&nbsp;</span>
+                                                <span className="item_element"><a href="#" onClick={handleLinkClick}>{item.target_name}</a></span></p></div>)
+                                            : (item.target_name === "" ? (null) : (<p className="item_3">{item.target_name}</p>))}
+                                        {(selectedIndex === item.event_id && item.alert_template !== '') ? (<div className="container"><p><span className="title_element">Шаблон сигнала:&nbsp;</span><span className="item_element">{item.alert_template}</span></p></div>) : (null)}
+
+                                        {(selectedIndex === item.event_id && item.value.emp !== '') ? (<div className="container"><p><span className="title_element">Значение:&nbsp;</span><span className="item_element">{item.value} {item.units}</span></p></div>) : (null)}
+
+                                        {(selectedIndex === item.event_id && item.label !== '') ?
+                                            (<div className="container"><p><span className="title_element">Класс опасности:&nbsp;</span>
+                                                <span className="item_element">{item.label}</span></p></div>) : (item.label === '' ? (null) : (<p className="item_5">Класс опасности: {item.label}</p>))}
+
+                                        {(selectedIndex === item.event_id && item.time_value !== '') ?
+                                            (<div className="container"><p><span className="title_element">Время:&nbsp;</span>
+                                                <span className="item_element">{timeConverter(item.time_value, props.filter)}</span></p></div>) : (item.time_value === '' ? (null) : (<p className="item_4">{timeConverter(item.time_value, props.filter)}</p>))}
+                                    </div>
+                                </>
+
                             </div>
-                            {/* </div> */}
-                            <div className="text_group">
-                                <div>
-                                    <p className="item_1">{MatchMapping[item.state].description}</p>
-                                    <p className="item_2">{item.name}</p>
-                                    <p className="item_3">{item.target_name}</p>
-                                    <p className="item_5">Класс опасности: {item.label}</p>
-                                    {props.filter === 'withinHour' ? (<p className="item_4">{minutesAgo}  мин назад</p>) : (null)}
-                                    {props.filter === 'today' ? (<p className="item_4">{formattedTime}</p>) : (null)}
-                                    {props.filter === 'yesterday' ? (<p className="item_4">{formattedDate}</p>) : (null)}
-                                </div>
-                            </div>
-                        </div>
+                        </>
                     )
                 })
             )}
+
+            {showPopupLink && (
+                <div className="popup chart">
+                    <button className="close-button" onClick={handleCloseLinkClick}>
+                        <div className="close_popup_btn">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16"><path d="m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z" /></svg>
+                        </div>
+                    </button>
+                    <h2>График</h2>
+                </div>
+            )}
+
         </div>
-    );
-}
+    )
+};
+
 
 export default Cards;
--- src/components/CollapseMenu.jsx
+++ src/components/CollapseMenu.jsx
@@ -8,6 +8,7 @@ import FilterPopup from './FilterPopup';
 import ColorTypeFilter from './ColorTypeFilter';
 import TextAreaFilter from './TextAreaFilter';
 import MatchMapping from "../MatshMapping";
+import SidenavBody from 'rsuite/esm/Sidenav/SidenavBody';
 
 
 
@@ -75,6 +76,7 @@ const CollapseMenu = () => {
     // Отфильтрованные по тексту данные
     const [filteredByText, setFilterText] = useState([]);
 
+
     const processText = () => {
         var buffValue = document.getElementById('elem1').value
         setInputText(buffValue);
@@ -92,24 +94,23 @@ const CollapseMenu = () => {
 
     if (result.length > 0) {
         if ((selectedFilters.length > 0) && (inputText !== '')) {
-            for (let i = 0; i < selectedFilters.length; i += 4) {
-                groupedData.push(selectedFilters.slice(i, i + 4));
-            }
             propToPass.length = 0;
             propToPass = propToPass.concat(TextAreaFilter(inputText, filterData))
         } else {
             if (selectedFilters.length > 0) {
                 propToPass.length = 0;
                 propToPass = propToPass.concat(filterData);
-                for (let i = 0; i < selectedFilters.length; i += 4) {
-                    groupedData.push(selectedFilters.slice(i, i + 4));
-                }
+
             }
             if (inputText !== '') {
                 propToPass.length = 0;
                 propToPass = propToPass.concat(TextAreaFilter(inputText, result));
             }
         }
+    }
+
+    for (let i = 0; i < selectedFilters.length; i += 4) {
+        groupedData.push(selectedFilters.slice(i, i + 4));
     }
 
     const totalPages = Math.ceil(propToPass.length / ITEMS_PER_PAGE);
@@ -154,7 +155,7 @@ const CollapseMenu = () => {
                 setPage(newValue);
                 setPrevValue(newValue);
             }
-        }, 2000);
+        }, 1000);
 
     };
 
@@ -204,78 +205,109 @@ const CollapseMenu = () => {
                         </div>
                     </div>
                 </div>
+                <div className='top_menu_wrapper'>
+                    <Sidenav.Body>
+                        <Nav>
+                            {!collapsed && (
+                                <>
+                                    <div className="top_menu">
+                                        <div className="filter_group">
+                                            <div className="search">
+                                                <div className="search_group">
+                                                    <button className="loupe_btn" onClick={processText}>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" className="search_svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                                            <path d="M9.5 3C11.2239 3 12.8772 3.68482 14.0962 4.90381C15.3152 6.12279 16 7.77609 16 9.5C16 11.11 15.41 12.59 14.44 13.73L14.71 14H15.5L20.5 19L19 20.5L14 15.5V14.71L13.73 14.44C12.59 15.41 11.11 16 9.5 16C7.77609 16 6.12279 15.3152 4.90381 14.0962C3.68482 12.8772 3 11.2239 3 9.5C3 7.77609 3.68482 6.12279 4.90381 4.90381C6.12279 3.68482 7.77609 3 9.5 3ZM9.5 5C7 5 5 7 5 9.5C5 12 7 14 9.5 14C12 14 14 12 14 9.5C14 7 12 5 9.5 5Z" fill="#93959A" />
+                                                        </svg>
+                                                    </button>
+                                                    <div className="search_title"><input className="text_area"
+                                                        type="text"
+                                                        id="elem1"
 
-                <Sidenav.Body>
-                    <Nav id="menuItems">
-                        {!collapsed && (
-                            <>
-                                <div className="top_menu">
-                                    <div className="filter_group">
-                                        <div className="search">
-                                            <div className="search_group">
-                                                <button className="loupe_btn" onClick={processText}>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" className="search_svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                                        <path d="M9.5 3C11.2239 3 12.8772 3.68482 14.0962 4.90381C15.3152 6.12279 16 7.77609 16 9.5C16 11.11 15.41 12.59 14.44 13.73L14.71 14H15.5L20.5 19L19 20.5L14 15.5V14.71L13.73 14.44C12.59 15.41 11.11 16 9.5 16C7.77609 16 6.12279 15.3152 4.90381 14.0962C3.68482 12.8772 3 11.2239 3 9.5C3 7.77609 3.68482 6.12279 4.90381 4.90381C6.12279 3.68482 7.77609 3 9.5 3ZM9.5 5C7 5 5 7 5 9.5C5 12 7 14 9.5 14C12 14 14 12 14 9.5C14 7 12 5 9.5 5Z" fill="#93959A" />
-                                                    </svg>
-                                                </button>
-                                                <div className="search_title"><input className="text_area"
-                                                    type="text"
-                                                    id="elem1"
-
-                                                    placeholder={'Поиск'}
-                                                /></div>
+                                                        placeholder={'Поиск'}
+                                                    /></div>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div className="filter_btn" onClick={openPopup}>
-                                            {selectedFilters.length !== 0 ? (
-                                                <svg xmlns="http://www.w3.org/2000/svg" className="filter_svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M13.5996 3H4.99986C4.77986 3 4.56986 3.08 4.37986 3.22C3.94986 3.56 3.86986 4.19 4.20986 4.62L9.96986 12H9.99986V17.87C9.95986 18.16 10.0599 18.47 10.2899 18.7L12.2999 20.71C12.6899 21.1 13.3199 21.1 13.7099 20.71C13.9399 20.5 14.0399 20.18 13.9999 19.88V12H14.0299L15.503 10.1126C13.9961 9.13152 12.9999 7.43211 12.9999 5.5C12.9999 4.59978 13.2162 3.75007 13.5996 3ZM16.0504 3C15.4021 3.63526 14.9999 4.52066 14.9999 5.5C14.9999 6.79177 15.6997 7.92009 16.7409 8.52653L19.7899 4.62C20.1299 4.19 20.0499 3.56 19.6199 3.22C19.4299 3.08 19.2199 3 18.9999 3H16.0504Z" fill="#93959A" />
-                                                    <circle cx="18.5" cy="5.5" r="3.5" fill="#0079F7" />
-                                                </svg>
-                                            ) : (
-                                                <svg xmlns="http://www.w3.org/2000/svg" className="filter_svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                                    <path d="M13.9999 12V19.88C14.0399 20.18 13.9399 20.5 13.7099 20.71C13.3199 21.1 12.6899 21.1 12.2999 20.71L10.2899 18.7C10.0599 18.47 9.95986 18.16 9.99986 17.87V12H9.96986L4.20986 4.62C3.86986 4.19 3.94986 3.56 4.37986 3.22C4.56986 3.08 4.77986 3 4.99986 3H18.9999C19.2199 3 19.4299 3.08 19.6199 3.22C20.0499 3.56 20.1299 4.19 19.7899 4.62L14.0299 12H13.9999Z" fill="#93959A" />
-                                                </svg>
+                                            <div className="filter_btn" onClick={openPopup}>
+                                                {selectedFilters.length !== 0 ? (
+                                                    <svg xmlns="http://www.w3.org/2000/svg" className="filter_svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                                        <path fillRule="evenodd" clipRule="evenodd" d="M13.5996 3H4.99986C4.77986 3 4.56986 3.08 4.37986 3.22C3.94986 3.56 3.86986 4.19 4.20986 4.62L9.96986 12H9.99986V17.87C9.95986 18.16 10.0599 18.47 10.2899 18.7L12.2999 20.71C12.6899 21.1 13.3199 21.1 13.7099 20.71C13.9399 20.5 14.0399 20.18 13.9999 19.88V12H14.0299L15.503 10.1126C13.9961 9.13152 12.9999 7.43211 12.9999 5.5C12.9999 4.59978 13.2162 3.75007 13.5996 3ZM16.0504 3C15.4021 3.63526 14.9999 4.52066 14.9999 5.5C14.9999 6.79177 15.6997 7.92009 16.7409 8.52653L19.7899 4.62C20.1299 4.19 20.0499 3.56 19.6199 3.22C19.4299 3.08 19.2199 3 18.9999 3H16.0504Z" fill="#93959A" />
+                                                        <circle cx="18.5" cy="5.5" r="3.5" fill="#0079F7" />
+                                                    </svg>
+                                                ) : (
+                                                    <svg xmlns="http://www.w3.org/2000/svg" className="filter_svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                                        <path d="M13.9999 12V19.88C14.0399 20.18 13.9399 20.5 13.7099 20.71C13.3199 21.1 12.6899 21.1 12.2999 20.71L10.2899 18.7C10.0599 18.47 9.95986 18.16 9.99986 17.87V12H9.96986L4.20986 4.62C3.86986 4.19 3.94986 3.56 4.37986 3.22C4.56986 3.08 4.77986 3 4.99986 3H18.9999C19.2199 3 19.4299 3.08 19.6199 3.22C20.0499 3.56 20.1299 4.19 19.7899 4.62L14.0299 12H13.9999Z" fill="#93959A" />
+                                                    </svg>
+                                                )}
+                                            </div>
+                                            {isPopupOpen && (
+                                                <FilterPopup onClose={closePopup} onApply={applyFilters} selectedOptions={selectedFilters} />
                                             )}
                                         </div>
-                                        {isPopupOpen && (
-                                            <FilterPopup onClose={closePopup} onApply={applyFilters} selectedOptions={selectedFilters} />
-                                        )}
-                                    </div>
-                                </div >
-                                {selectedFilters.length !== 0 ? (
-                                    <div>
-                                        {groupedData.map((element) => {
-                                            return (
-                                                <div className="selected_filters">
-                                                    {element.map((item, itemIndex) => (
-                                                        <div className="box_filter" key={itemIndex}>
-                                                            {item.match(/^\d+$/) ? (<div className="signal" style={{ background: MatchMapping[item].color }}></div>) :
-                                                                <div className="signal" style={{ background: '#FFFFFF' }} > {item}</div>}
-                                                            < button className='cancel_btn'
-                                                                onClick={() => cancelFilter(itemIndex)}
-                                                            >
-                                                                <div className="cancel_div" >
-                                                                    <div className="cancel_svg">
-                                                                        <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16"><path d="m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z" /></svg>
-                                                                    </div>
-                                                                </div>
-                                                            </button>
-                                                        </div >
-                                                    ))}
-                                                </div>
-                                            )
-                                        })
-                                        }
                                     </div >
-                                ) : (null)}
+                                </>
+                            )}
+                        </Nav>
+                    </Sidenav.Body>
+                </div>
+                <div className='super_main_wrapper'>
+                    <Sidenav.Body>
+                        <Nav id="menuItems">
+                            {!collapsed && (
+                                <>
+                                    {selectedFilters.length !== 0 ? (
+                                        <>
 
-                                <div className='main_cards_wrapper'>
+                                            {groupedData.map((element) => {
+                                                return (
+                                                    <div className="selected_filters">
+                                                        {element.map((item, itemIndex) => (
+                                                            <div className="box_filter" key={itemIndex}>
+                                                                {item.match(/^\d+$/) ? (<div className="signal" style={{ background: MatchMapping[item].color }}></div>) :
+                                                                    <div className="signal" style={{ background: '#FFFFFF' }} > {item}</div>}
+                                                                < button className='cancel_btn'
+                                                                    onClick={() => cancelFilter(itemIndex)}
+                                                                >
+                                                                    <div className="cancel_div" >
+                                                                        <div className="cancel_svg">
+                                                                            <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16"><path d="m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z" /></svg>
+                                                                        </div>
+                                                                    </div>
+                                                                </button>
+                                                            </div >
+                                                        ))}
+                                                    </div>
+                                                )
+                                            })
+                                            }
+                                        </>
+                                    ) : (null)}
+                                </>
+                            )}
+                        </Nav>
+                    </Sidenav.Body>
+                </div>
+
+
+                <div className="main_cards_wrapper">
+                    <Sidenav.Body>
+                        <Nav id="menuItems">
+                            {!collapsed && (
+                                <>
                                     <div className='cards_wrapper'>
-                                        <Cards itemValue={currentItems} filter={selectedOption} />
+                                        <div className='cards'>
+                                            <Cards itemValue={currentItems} filter={selectedOption} />
+                                        </div>
                                     </div>
-                                </div>
+                                </>
+                            )}
+                        </Nav>
+                    </Sidenav.Body>
+                </div>
 
+                <div className='main_pagination_wrapper'>
+                    <Sidenav.Body>
+                        {!collapsed && (
+                            <>
                                 {totalPages > 1 ?
                                     <div className="pagination-wrapper">
                                         <button className='page_btn bottom_line_element' onClick={() => handlePageChangeBtn(-2)}>
@@ -301,13 +333,10 @@ const CollapseMenu = () => {
                                         <div className='bottom_line_element'>из</div>
                                         <div className='bottom_line_element'>{totalPages}</div>
                                     </div> : null}
-
                             </>
                         )}
-                    </Nav>
-                </Sidenav.Body>
-
-
+                    </Sidenav.Body>
+                </div>
             </Sidenav>
         </Sidebar>
     );
